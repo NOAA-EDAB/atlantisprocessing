9bec5c6e0a4e529fa2367d0f0a3bb69a0cf0ba26
andybeet <22455149+andybeet@users.noreply.github.com>
2023-06-09 10:37:19 -0400

    add get_atl_paramfiles. - split out helper function to own file

---
+++ R/helperfiles.r
@@ -0,0 +1,14 @@
+#' Helper files
+#'
+#' Internal to package
+#' Generate paths of param files from run command
+#'
+#' @param run.cmd Content of run (bat) file
+#' @param code flag that denotes parameter file
+#'
+#' @noRd
+
+
+run.filename <- function(run.cmd,code){
+  return(strsplit(strsplit(run.cmd,paste0(code,' '))[[1]][2],' ')[[1]][1])
+}
